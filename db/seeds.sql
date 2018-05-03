72618fc00630187e65006c5458c2447c5dd8a330
Kyle Thomas <34077284+kthomas0455@users.noreply.github.com>
2018-05-02 18:23:33 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double no cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey', true);
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon');
